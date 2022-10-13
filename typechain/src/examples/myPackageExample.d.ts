29b3398e9e7b8244aa0933537dd148476ef534e5
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-10-13 18:10:27 +0900

    Add: typechain

---
+++ typechain/src/examples/myPackageExample.d.ts
@@ -0,0 +1,10 @@
+// import { init, exit } from "myPackage";
+
+interface Config {
+  url: string;
+}
+
+declare module "myPackage" {
+  function init(config: Config): boolean;
+  function exit(code: number): number;
+}
